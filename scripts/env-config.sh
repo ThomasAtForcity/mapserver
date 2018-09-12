2bad2c5878d9f1869fd9de41d210a0cb865eb253
Thomas Lion <thomas.lion@forcity.com>
2018-09-12 17:21:42 +0200

    Refactoring

--- scripts/env-config.sh
+++ scripts/env-config.sh
@@ -2,13 +2,13 @@
 # CREATE a config mapserver mapfile
 DIST_DIR="/etc/mapserver/dist/"
 mkdir -p $DIST_DIR 
-CONFIG_DEST_FILE="$DIST_DIR/config.map"
+CONFIG_DEST_FILE="$DIST_DIR/db-connection.map"
 
-if [ -z "${DB_USER}" -o -z "${DB_PW}" -o -z "${DB_NAME}" -o -z "${DB_HOST}" ]
+if [ -z "${URB_MS_DB_USER}" -o -z "${URB_MS_DB_PW}" -o -z "${URB_MS_DB_NAME}" -o -z "${URB_MS_DB_HOST}" ]
 then
-  echo "ERROR env variables are not all specify (DB_USER, DB_PW, DB_NAME, DB_HOST) !!!!"
+  echo "ERROR env variables are not all specify (URB_MS_DB_USER, URB_MS_DB_PW, URB_MS_DB_NAME, URB_MS_DB_HOST) !!!!"
 else
   # Create mapfile config  file
-  echo "CONNECTION 'dbname=$DB_NAME user=$DB_USER password=$DB_PW host=$DB_HOST'" > $CONFIG_DEST_FILE
+  echo "CONNECTION 'dbname=$URB_MS_DB_NAME user=$URB_MS_DB_USER password=$URB_MS_DB_PW host=$URB_MS_DB_HOST'" > $CONFIG_DEST_FILE
   echo "INFO env. variables handler : OK"
 fi
